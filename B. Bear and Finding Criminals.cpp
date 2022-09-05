3b17007cec6d2adf837ded85569d4a85073354f7
Rushikesh Langde <74859623+Ross0121@users.noreply.github.com>
2022-09-05 14:00:20 +0530

    Create B. Bear and Finding Criminals.cpp

---
+++ B. Bear and Finding Criminals.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+int n,a;
+cin>>n>>a;
+a--;
+int ar[n];
+for(int i=0;i<n;i++)
+    cin>>ar[i];
+int c=0;
+for(int j=0;j<n;j++)
+{
+    if(a+j<n&&a-j>=0)
+    {
+        if(ar[a-j]==1&&ar[a+j]==1&&j!=0){c+=2;}
+        else if(ar[a-j]==1&&ar[a+j]==1&&j==0){c+=1;}
+    }
+     else if(a-j<0&&a+j<n)
+    {
+        if(ar[a+j]==1)c++;
+        // cout<<"1"<<"\n";
+    }
+    else if(a+j>=n&&a-j>=0)
+    {
+        if(ar[a-j]==1)c++;
+        // cout<<"2"<<"\n";
+    }
+}
+cout<<c<<"\n";
+return 0;
+}
